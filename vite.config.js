3f2aea30e83328a9484740d19477aab25c00ea10
Anton Sukhochenkov <sukhochenkov@gmail.com>
2025-08-26 22:56:27 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -33,28 +33,28 @@ export default defineConfig({
     rollupOptions: {
       input: {
         "inject-shadow-ui": path.resolve(__dirname, 'src/content-scripts/inject-shadow-ui.jsx'),
-        "content-linkedin-scraper": path.resolve(__dirname, 'src/content-scripts/content-linkedin-scraper.js'),
-        "content-total-counter": path.resolve(__dirname, "src/content-scripts/content-total-counter.js"),
-        "content-dismissed": path.resolve(__dirname, "src/content-scripts/content-dismissed.js"),
-        "content-promoted": path.resolve(__dirname, "src/content-scripts/content-promoted.js"),
-        "content-applied": path.resolve(__dirname, "src/content-scripts/content-applied.js"),
-        "content-viewed": path.resolve(__dirname, "src/content-scripts/content-viewed.js"),
-        "content-keywords": path.resolve(__dirname, "src/content-scripts/content-keywords.js"),
-        "content-hours-patch": path.resolve(__dirname, "src/content-scripts/content-hours-patch.js"),
-        "content-companies": path.resolve(__dirname, "src/content-scripts/content-companies.js"),
 
-        "content-applied-indeed": path.resolve(__dirname, "src/content-scripts/content-applied-indeed.js"),
-        "content-sponsored-indeed": path.resolve(__dirname, "src/content-scripts/content-sponsored-indeed.js"),
-        "content-companies-indeed": path.resolve(__dirname, "src/content-scripts/content-companies-indeed.js"),
-        "content-keywords-indeed": path.resolve(__dirname, "src/content-scripts/content-keywords-indeed.js"),
-        "content-total-counter-indeed": path.resolve(__dirname, "src/content-scripts/content-total-counter-indeed.js"),
+        "content-linkedin-scraper": path.resolve(__dirname, 'src/content-scripts/linkedin-content/content-linkedin-scraper.js'),
+        "content-total-counter": path.resolve(__dirname, "src/content-scripts/linkedin-content/content-total-counter.js"),
+        "content-dismissed": path.resolve(__dirname, "src/content-scripts/linkedin-content/content-dismissed.js"),
+        "content-promoted": path.resolve(__dirname, "src/content-scripts/linkedin-content/content-promoted.js"),
+        "content-applied": path.resolve(__dirname, "src/content-scripts/linkedin-content/content-applied.js"),
+        "content-viewed": path.resolve(__dirname, "src/content-scripts/linkedin-content/content-viewed.js"),
+        "content-keywords": path.resolve(__dirname, "src/content-scripts/linkedin-content/content-keywords.js"),
+        "content-hours-patch": path.resolve(__dirname, "src/content-scripts/linkedin-content/content-hours-patch.js"),
+        "content-companies": path.resolve(__dirname, "src/content-scripts/linkedin-content/content-companies.js"),
+
+        "content-applied-indeed": path.resolve(__dirname, "src/content-scripts/indeed-content/content-applied-indeed.js"),
+        "content-sponsored-indeed": path.resolve(__dirname, "src/content-scripts/indeed-content/content-sponsored-indeed.js"),
+        "content-companies-indeed": path.resolve(__dirname, "src/content-scripts/indeed-content/content-companies-indeed.js"),
+        "content-keywords-indeed": path.resolve(__dirname, "src/content-scripts/indeed-content/content-keywords-indeed.js"),
+        "content-total-counter-indeed": path.resolve(__dirname, "src/content-scripts/indeed-content/content-total-counter-indeed.js"),
         
 
-        "content-keywords-glassdoor": path.resolve(__dirname, "src/content-scripts/content-keywords-glassdoor.js"),
-        "content-applied-glassdoor": path.resolve(__dirname, "src/content-scripts/content-applied-glassdoor.js"),
-        "content-companies-glassdoor": path.resolve(__dirname, "src/content-scripts/content-companies-glassdoor.js"),
-        "content-total-counter-glassdoor": path.resolve(__dirname, "src/content-scripts/content-total-counter-glassdoor.js"),
-
+        "content-keywords-glassdoor": path.resolve(__dirname, "src/content-scripts/glassdoor-content/content-keywords-glassdoor.js"),
+        "content-applied-glassdoor": path.resolve(__dirname, "src/content-scripts/glassdoor-content/content-applied-glassdoor.js"),
+        "content-companies-glassdoor": path.resolve(__dirname, "src/content-scripts/glassdoor-content/content-companies-glassdoor.js"),
+        "content-total-counter-glassdoor": path.resolve(__dirname, "src/content-scripts/glassdoor-content/content-total-counter-glassdoor.js"),
       },
       output: {
         entryFileNames: '[name].js'
